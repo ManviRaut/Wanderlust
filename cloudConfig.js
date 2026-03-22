ba8ece46f26f7a3e4233cd0baf7ebd981f57266e
ManviRaut <manviraut30@gmail.com>
2026-03-22 22:17:05 +0530

    Update cloudConfig.js

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,39 +1,23 @@
 const cloudinary = require('cloudinary').v2;
-const {CloudinaryStorage} = require('multer-storage-cloudinary');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-cloudinary.config(
-    {
+cloudinary.config({
         cloud_name: process.env.CLOUD_NAME,
         api_key: process.env.CLOUDE_API_KEY,
         api_secret: process.env.CLOUDE_API_SECRET
-    }
-);
-
-// const storage = new CloudinaryStorage({
-//     cloudinary: cloudinary,
-//     params: {
-//         folder: 'wanderlust_DEV',
-//         allowedFormats: ["png", "jpg", "jpeg"],
-
-//     },
-// });
-// module.exports = {
-//     cloudinary,
-//     storage,
-// }
-
-const storage = new CloudinaryStorage({
-    cloudinary,
-    params: async (req, file) => {
-        return {
-            folder: 'wanderlust_DEV',
-            resource_type: 'image',
-            format: file.mimetype.split('/')[1], // auto-detect (jpg, png, etc.)
-        };
-    },
 });
 
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'wanderlust_DEV',
+        allowed_formats: ["png", "jpg", "jpeg"],
+
+    },
+});
 module.exports = {
     cloudinary,
-    storage,
+    storage
 };
+
+

acd5757dc38029bda4bcfa756e6959a8cd687b85
ManviRaut <manviraut30@gmail.com>
2026-03-22 22:19:05 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -9,11 +9,11 @@ module.exports.ListingSchema = Joi.object({
         price: Joi.number().required().min(0),
         image: Joi.string().allow("", null),
     }).required(),
-})
+});
 
 module.exports.ReviewSchema = Joi.object({
     review: Joi.object({
         rating: Joi.number().required().min(1).max(5),
         comment: Joi.string().required(),
     }).required(),
-})
+});

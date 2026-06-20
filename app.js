ab1d8339ff0188cb46ce2164865f7aa43a3d85de
ManviRaut <manviraut30@gmail.com>
2026-06-20 22:35:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
     res.redirect("/listing");
 });
-app.use("/listing", listings);
+app.use("/listings", listings);
 app.use("/listings/:id/reviews", reviews);
 app.use("/", userRouter);
 

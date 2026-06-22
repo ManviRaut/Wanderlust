6eee0bb87647896193aa7eed84100ce7ac1e7c63
ManviRaut <manviraut30@gmail.com>
2026-06-22 23:48:54 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -16,34 +16,8 @@ router.route("/")
 //New Route
 router.get("/newForm", isloggedin, listingController.RenderNewForm);
 //search route
-router.get("/search", async (req, res) => {
-    const query = req.query.q;
 
-    const listings = await Listing.find({
-        $or: [
-            {
-                title: {
-                    $regex: query,
-                    $options: "i",
-                },
-            },
-            {
-                location: {
-                    $regex: query,
-                    $options: "i",
-                },
-            },
-            {
-                country: {
-                    $regex: query,
-                    $options: "i",
-                },
-            },
-        ],
-    });
-
-    res.render("index.ejs", { allListing: listings });
-});
+           
 //Edit route
 router.get("/:id/edit", isloggedin, isOwner, wrapAsync(listingController.editListing));
 router.route("/:id")

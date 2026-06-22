63d4065e98c9991112532e691235dd4dff9fd8d8
ManviRaut <manviraut30@gmail.com>
2026-06-23 00:49:00 +0530

    Update listing.js

--- controller/listing.js
+++ controller/listing.js
@@ -1,8 +1,8 @@
 const Listing = require("../models/listing");
 
 module.exports.index = async (req, res) => {
-    const allListings = await Listing.find({});
-  res.render("index", { allListings });
+    const allListing = await Listing.find({});
+  res.render("index", { allListing });
 }; 
 
 module.exports.RenderNewForm = (req, res) => {
